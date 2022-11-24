f5cebdd112c80a3b9f4df18160c15c8e73bef98b
kpedrozo <kayp_22@hotmail.com>
2022-11-24 16:19:10 -0300

    change burger menu align rigth

--- grupo_03/nav-hamburgesa.js
+++ grupo_03/nav-hamburgesa.js
@@ -3,8 +3,10 @@ const burgerMenuItems = document.getElementById("burger-menu")
 const nav = document.querySelector("nav");
 const container = nav.firstElementChild;
 const burgerMenu = document.querySelector(".burger-nav");
+const datosUsuario = document.getElementById("datosUsuario")
 burgerMenu.addEventListener("click", () => {
   burgerMenuItems.style.display = "block";
+
 });
 burgerMenuItems.addEventListener("click", () => {
   burgerMenuItems.style.display = "none";
